8d9dae8bffd218143805970f239b7f9ea70554c0
SilasB <97103478@st.deltion.nl>
2025-03-05 15:38:13 +0100

    klaar1

--- opdrachten/010_variabelen/opdr_3/opdr_3.py
+++ opdrachten/010_variabelen/opdr_3/opdr_3.py
@@ -1,9 +1,8 @@
 # Opdracht 1
-# Naam student:
-# Groep:
+# Naam student:SilasBrinkman
+# Groep:IT2B
 
 # Hier komt je code...
 
-my_dict = ...
-
-print()
+my_dict = {"willem": "van der broek", "23": 8, "blauw": "groen"}
+print(my_dict)
