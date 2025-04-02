d4fc2d3156b329983a55bf2a0ed0602143e74815
SilasB <97103478@st.deltion.nl>
2025-04-02 14:51:14 +0200

    Klaar

--- opdrachten/060_loops/opdr_1/opdr_1.py
+++ opdrachten/060_loops/opdr_1/opdr_1.py
@@ -1,9 +1,10 @@
 # Opdracht 1 loops
-# Naam student:
-# Groep:
+# Naam student: Silas Brinkman
+# Groep: IT2B
 
 # Hier komt je code, maak gebruik van de input functie om de lengte van de rechthoekzijden van de driehoek op te vragen.
 
 # Hier start de for-loop....
 
-my_list = []
+getallen = [i for i in range(1, 11)]
+print(getallen)
