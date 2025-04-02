24d4e5743c18b685c29eeb15f461781c99308e1f
SilasB <97103478@st.deltion.nl>
2025-04-02 14:59:07 +0200

    Klaar

--- opdrachten/060_loops/opdr_6/opdr_6.py
+++ opdrachten/060_loops/opdr_6/opdr_6.py
@@ -4,6 +4,13 @@
 
 # Hier komt je code...
 
-# Hier start de for-loop
-
-my_list = []
+pizzas = ['margharita', 'calzone', 'verdi', 'olivio', 'quattro stagioni']
+pizzas.sort()
+print("Gesorteerd:", pizzas)
+pizzas.append('yo-favorito')
+print("Met favoriet:", pizzas)
+pizzas.remove('olivio')
+print("Zonder minst favoriet:", pizzas)
+print("Eerste 3:", pizzas[:3])
+print("Middelste:", [pizzas[2]])
+print("Laatste 3:", pizzas[-3:])
