2fdc584d1d795dbd8bab83e97cab588a7c6868ac
SilasB <97103478@st.deltion.nl>
2025-04-02 15:20:50 +0200

    Klaar

--- opdrachten/080_tekstbestanden/opdr_3/opdr_3.py
+++ opdrachten/080_tekstbestanden/opdr_3/opdr_3.py
@@ -1,6 +1,28 @@
 # Opdracht 3 Tekst opslaan
-# Naam student:
-# Groep:
+# Naam student: Silas Brinkman
+# Groep: IT2B
 
+def encrypt(tekst):
+    resultaat = ""
+    for letter in tekst:
+        if letter.isalpha():
+            is_hoofdletter23 = letter.isupper()
+            code = ord(letter.lower())
+            verschoven = code + 5
+            
+            if verschoven > ord('z'):
+                verschoven = verschoven - 26
+                
+            nieuwe_letter = chr(verschoven)
+            if is_hoofdletter23:
+                nieuwe_letter = nieuwe_letter.upper()
+            resultaat += nieuwe_letter
+        else:
+            resultaat += letter
+    return resultaat
 
+invoer = input("Geef de tekst die wilt encrypten..\n")
+
+geencrypteerd = encrypt(invoer)
+print(geencrypteerd)
 
