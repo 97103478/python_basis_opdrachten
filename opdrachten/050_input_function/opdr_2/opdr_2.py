760c23dcb999b8868912207113d1adb3a8a4164e
SilasB <97103478@st.deltion.nl>
2025-04-02 14:21:21 +0200

    Klaar

--- opdrachten/050_input_function/opdr_2/opdr_2.py
+++ opdrachten/050_input_function/opdr_2/opdr_2.py
@@ -1,7 +1,17 @@
 # Opdracht 2 berekeningen
-# Naam student:
-# Groep:
+# Naam student: Silas Brinkman
+# Groep: IT2B
 
 # Hier komt je code...
 
-gasten = ["Jij", ... ]
+gasten = ["Paul", "Kees", "Marie", "Hilda"]
+
+gasten.insert(0, "Silas")
+print("Originele lijst:", gasten)  # ["Silas", "Paul", "Kees", "Marie", "Hilda"]
+
+gasten.remove("Marie")
+print("Na afzegging Marie:", gasten)  # ["Silas", "Paul", "Kees", "Hilda"]
+
+kees_index = gasten.index("Kees")  # Vind de positie van Kees
+gasten.insert(kees_index + 1, "George")  # Voeg George toe na Kees
+print("Met George erbij:", gasten)  # ["Silas", "Paul", "Kees", "George", "Hilda"]
