0cd62add73a1e60eb4842b90dbea70e74adc1b14
SilasB <97103478@st.deltion.nl>
2025-04-02 15:23:37 +0200

    Klaar

--- opdrachten/080_tekstbestanden/opdr_4/opdr_4.py
+++ opdrachten/080_tekstbestanden/opdr_4/opdr_4.py
@@ -1,6 +1,32 @@
 # Opdracht 4 Tekst opslaan
-# Naam student:
-# Groep:
+# Naam student: Silas Brinkman
+# Groep: IT2B
 
+vragen = [
+    "Wat is je voornaam?",
+    "Wat is je achternaam?",
+    "Wat neem je mee aan drank?",
+    "Wat neem je mee om te eten?"
+]
 
-# Party enquete
+feestganger = {}
+
+for i, vraag in enumerate(vragen, 1): 
+    antwoord = input(f"{i}. {vraag} \n")
+    if i == 1:
+        feestganger["voornaam"] = antwoord
+    elif i == 2:
+        feestganger["achternaam"] = antwoord
+    elif i == 3:
+        feestganger["drank"] = antwoord
+    else:
+        feestganger["eten"] = antwoord
+
+with open("party_gasten.txt", "a") as bestand:
+    bestand.write("----\n")
+    for key, value in feestganger.items():
+        bestand.write(f"{key}: {value}\n")
+    bestand.write("\n")
+
+print("\nBedankt voor het invullen!")
+print("See you at the party.")
