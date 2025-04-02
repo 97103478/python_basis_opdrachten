ef7a4f1a7db294b63ab96fab426304540b5f7da7
SilasB <97103478@st.deltion.nl>
2025-04-02 15:03:18 +0200

    Klaar

--- opdrachten/070_condities/opdr_2/opdr_2.py
+++ opdrachten/070_condities/opdr_2/opdr_2.py
@@ -1,13 +1,13 @@
 # Opdracht 2 condities
-# Naam student:
-# Groep:
+# Naam student: Silas Brinkman
+# Groep: IT2B
 
-# Hier komt je code...
+getallen = [43948, 878768, 38768, 87555, 765765]
 
-# Hier start de for-loop
+deelbaar_door_3 = []
 
-my_list = [43948, 878768, 38768, 87555, 765765]
+for getal in getallen:
+    if getal % 3 == 0: 
+        deelbaar_door_3.append(getal)
 
-# for...:
-#   if...:
-#       print()
+print(deelbaar_door_3)
