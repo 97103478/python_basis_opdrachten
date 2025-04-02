a20c33d2c7bd3b6bfe9a2435ce3687cfd6e8291a
SilasB <97103478@st.deltion.nl>
2025-04-02 14:52:29 +0200

    Klaar

--- opdrachten/060_loops/opdr_2/opdr_2.py
+++ opdrachten/060_loops/opdr_2/opdr_2.py
@@ -1,10 +1,10 @@
 # Opdracht 2 loops
-# Naam student:
-# Groep:
+# Naam student: Silas Brinkman
+# Groep: IT2B
 
 # Hier komt je code...
 
-# Hier start de for-loop
-
-my_list = []
-
+getallen = []
+for i in range(10, 51, 10):
+    getallen.append(i)
+print(getallen)
