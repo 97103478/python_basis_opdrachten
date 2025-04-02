930b83b40461879fef6876ffc80a113e7dabe5bc
SilasB <97103478@st.deltion.nl>
2025-04-02 15:16:06 +0200

    Klaar

--- opdrachten/080_tekstbestanden/opdr_1/opdr_1.py
+++ opdrachten/080_tekstbestanden/opdr_1/opdr_1.py
@@ -1,5 +1,23 @@
 # Opdracht 1 while-loops
-# Naam student:
-# Groep:
+# Naam student: Silas Brinkman
+# Groep: IT2B
 
-# Jouw code komt hier
+vragen = [
+    "Wat vind je van de huidige regering?",
+    "Wat vind je van de Python-lessen tot nu toe?",
+    "Wat vind jij de mooiste stad van Nederland?"
+]
+
+antwoorden = []
+
+for vraag in vragen:
+    antwoord = input(vraag + " ")
+    antwoorden.append(antwoord)
+
+with open("enquete_resultaten.txt", "w") as bestand:
+    for i in range(len(vragen)):
+        bestand.write(f"Vraag: {vragen[i]}\n")
+        bestand.write(f"Antwoord: {antwoorden[i]}\n")
+        bestand.write("-" * 30 + "\n")
+
+print("Bedankt voor het invullen! De antwoorden zijn opgeslagen in enquete_resultaten.txt")
